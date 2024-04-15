211949b4b68cba579480ce9a03177d2ea2827f06
ElianaCepeda <eli.cepedagonzalez@gmail.com>
2024-04-14 19:51:49 -0500

    Cambio en la implementacion de los singleton

--- src/main/java/omega/sgb/SingletonControladores.java
+++ src/main/java/omega/sgb/SingletonControladores.java
@@ -6,57 +6,25 @@ import omega.sgb.dominio.Persona;
 
 public class SingletonControladores {
     private static SingletonControladores myself;
-    private static SingletonPantallas sPantallas = SingletonPantallas.getInstance();
-    private Persona usuarioActual;
-    private ControladorPrestamo controladorPrestamo;
-    private ControladorLogIn controladorLogIn;
-
-    public static SingletonPantallas getsPantallas() {
-        return sPantallas;
-    }
-
-    public static void setsPantallas(SingletonPantallas sPantallas) {
-        SingletonControladores.sPantallas = sPantallas;
-    }
-
-    public Persona getUsuarioActual() {
+    private static Persona usuarioActual;
+    private static ControladorPrestamo controladorPrestamo;
+    private static ControladorLogIn controladorLogIn;
+    public static Persona getUsuarioActual() {
         return usuarioActual;
     }
 
-    public void setUsuarioActual(Persona usuarioActual) {
-        this.usuarioActual = usuarioActual;
-    }
 
-    public ControladorPrestamo getControladorPrestamo() {
-        return controladorPrestamo;
-    }
 
-    public void setControladorPrestamo(ControladorPrestamo controladorPrestamo) {
-        this.controladorPrestamo = controladorPrestamo;
-    }
-
-    public ControladorLogIn getControladorLogIn() {
+    public static ControladorLogIn getInstanceLogIn() {
+        if (controladorLogIn == null) {
+            controladorLogIn = new ControladorLogIn();
+        }
         return controladorLogIn;
     }
-
-    public void setControladorLogIn(ControladorLogIn controladorLogIn) {
-        this.controladorLogIn = controladorLogIn;
-    }
-
-    public SingletonControladores(){
-
-    }
-
-    public SingletonControladores(Persona usuarioActual, ControladorPrestamo controladorPrestamo, ControladorLogIn controladorLogIn) {
-        this.usuarioActual = usuarioActual;
-        this.controladorPrestamo = controladorPrestamo;
-        this.controladorLogIn = controladorLogIn;
-    }
-
-    public static SingletonControladores getInstance() {
-        if (myself == null) {
-            myself = new SingletonControladores();
+    public static ControladorPrestamo getInstancePrestamo() {
+        if (controladorPrestamo == null) {
+            controladorPrestamo = new ControladorPrestamo();
         }
-        return myself;
+        return controladorPrestamo;
     }
 }
--- src/main/java/omega/sgb/SingletonPantallas.java
+++ src/main/java/omega/sgb/SingletonPantallas.java
@@ -3,82 +3,45 @@ package omega.sgb;
 import omega.sgb.gui.*;
 
 public class SingletonPantallas {
-    private LoginGUI login;
-    private CrearCuentaGUI crearCuenta;
-    private BusquedaGUI busqueda;
-    private MainLectorGUI mainLector;
-    private MainBibliotecarioGUI mainBibliotecario;
-
-    private static SingletonPantallas myself;
-    private static SingletonControladores sControladores = SingletonControladores.getInstance();
+    private static LoginGUI login;
+    private static CrearCuentaGUI crearCuenta;
+    private static BusquedaGUI busqueda;
+    private static MainLectorGUI mainLector;
+    private static MainBibliotecarioGUI mainBibliotecario;
 
 
-    public LoginGUI getLogin() {
-        return login;
-    }
-
-    public void setLogin(LoginGUI login) {
-        this.login = login;
-    }
-
-    public CrearCuentaGUI getCrearCuenta() {
-        return crearCuenta;
-    }
-
-    public void setCrearCuenta(CrearCuentaGUI crearCuenta) {
-        this.crearCuenta = crearCuenta;
-    }
-
-    public BusquedaGUI getBusqueda() {
+    public static BusquedaGUI getInstanceBusqueda() {
+        if (busqueda == null) {
+            busqueda = new BusquedaGUI();
+        }
         return busqueda;
     }
 
-    public void setBusqueda(BusquedaGUI busqueda) {
-        this.busqueda = busqueda;
+    public static CrearCuentaGUI getInstanceCrearCuenta() {
+        if (crearCuenta == null) {
+            crearCuenta = new CrearCuentaGUI();
+        }
+        return crearCuenta;
     }
 
-    public MainLectorGUI getMainLector() {
-        return mainLector;
+    public static LoginGUI getInstanceLogin() {
+        if (login == null) {
+            login = new LoginGUI();
+        }
+        return login;
     }
 
-    public void setMainLector(MainLectorGUI mainLector) {
-        this.mainLector = mainLector;
-    }
-
-    public MainBibliotecarioGUI getMainBibliotecario() {
+    public static MainBibliotecarioGUI getInstanceMainBibliotecario() {
+        if (mainBibliotecario == null) {
+            mainBibliotecario = new MainBibliotecarioGUI();
+        }
         return mainBibliotecario;
     }
 
-    public void setMainBibliotecario(MainBibliotecarioGUI mainBibliotecario) {
-        this.mainBibliotecario = mainBibliotecario;
-    }
-
-
-    public static SingletonControladores getsControladores() {
-        return sControladores;
-    }
-
-    public static void setsControladores(SingletonControladores sControladores) {
-        SingletonPantallas.sControladores = sControladores;
-    }
-
-    public SingletonPantallas(){
-
-    }
-
-    public SingletonPantallas(LoginGUI login, CrearCuentaGUI crearCuenta, BusquedaGUI busqueda, MainLectorGUI mainLector, MainBibliotecarioGUI mainBibliotecario) {
-        this.login = login;
-        this.crearCuenta = crearCuenta;
-        this.busqueda = busqueda;
-        this.mainLector = mainLector;
-        this.mainBibliotecario = mainBibliotecario;
-    }
-
-    public static SingletonPantallas getInstance() {
-        if (myself == null) {
-            myself = new SingletonPantallas();
+    public static MainLectorGUI getInstanceMainLector() {
+        if (mainLector == null) {
+            mainLector = new MainLectorGUI();
         }
-        return myself;
+        return mainLector;
     }
-
 }
