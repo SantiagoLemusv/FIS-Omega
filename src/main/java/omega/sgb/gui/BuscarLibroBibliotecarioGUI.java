4accbdd5f7f33b5c68027723e61208d943981036
florezandres <andres.f1850@gmail.com>
2024-05-05 13:59:33 -0500

    comit all

--- src/main/java/omega/sgb/gui/BuscarLibroBibliotecarioGUI.java
+++ src/main/java/omega/sgb/gui/BuscarLibroBibliotecarioGUI.java
@@ -1,8 +1,11 @@
 package omega.sgb.gui;
 
+import javafx.event.ActionEvent;
 import omega.sgb.SingletonControladores;
+import omega.sgb.SingletonPantallas;
 import omega.sgb.control.ControladorBusquedaLibro;
 
+import java.io.IOException;
 import java.sql.SQLException;
 
 public class BuscarLibroBibliotecarioGUI {
@@ -10,5 +13,20 @@ public class BuscarLibroBibliotecarioGUI {
     public BuscarLibroBibliotecarioGUI() throws SQLException {}
     public BuscarLibroBibliotecarioGUI(ControladorBusquedaLibro controladorBusquedaLibro) throws SQLException {
         this.controladorBusquedaLibro = controladorBusquedaLibro;
+    }
+    public void mBtnMiPerfil(ActionEvent event) throws IOException {
+        SingletonPantallas.toEstadoBibliotecarioViewSingleton(event);
+    }
+    public void mBtnBusqueda(ActionEvent event) throws IOException {
+        SingletonPantallas.toBuscarLibroBibliotecarioViewSingleton(event);
+    }
+    public void mBtnCarrito(ActionEvent event) throws IOException {
+        SingletonPantallas.toCarritoViewSingleton(event);
+    }
+    public void mBtnCerrarSesion(ActionEvent event) throws IOException {
+        SingletonPantallas.toLogInViewSingleton(event);
+    }
+    public void mBtnBuscar(ActionEvent event) throws IOException {
+        SingletonPantallas.toResultadoLibroViewSingleton(event);
     }
 }
--- src/main/java/omega/sgb/gui/BuscarLibroLectorGUI.java
+++ src/main/java/omega/sgb/gui/BuscarLibroLectorGUI.java
@@ -1,9 +1,12 @@
 package omega.sgb.gui;
 
+import javafx.event.ActionEvent;
 import omega.sgb.SingletonControladores;
+import omega.sgb.SingletonPantallas;
 import omega.sgb.control.ControladorBusquedaLibro;
 import omega.sgb.control.ControladorLogIn;
 
+import java.io.IOException;
 import java.sql.SQLException;
 
 public class BuscarLibroLectorGUI {
@@ -11,5 +14,17 @@ public class BuscarLibroLectorGUI {
     public BuscarLibroLectorGUI() throws SQLException {}
     public BuscarLibroLectorGUI(ControladorBusquedaLibro controladorBusquedaLibro) throws SQLException {
         this.controladorBusquedaLibro = controladorBusquedaLibro;
+    }
+    public void mBtnMiPerfil(ActionEvent event) throws IOException {
+        SingletonPantallas.toEstadoLectorViewSingleton(event);
+    }
+    public void mBtnBusqueda(ActionEvent event) throws IOException {
+        SingletonPantallas.toBuscarLibroLectorViewSingleton(event);
+    }
+    public void mBtnCerrarSesion(ActionEvent event) throws IOException {
+        SingletonPantallas.toLogInViewSingleton(event);
+    }
+    public void mBtnBuscar(ActionEvent event) throws IOException {
+        SingletonPantallas.toResultadoLibroViewSingleton(event);
     }
 }
