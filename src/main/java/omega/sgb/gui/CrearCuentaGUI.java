a39187326218f3388a24a485256b4b96dd54ebad
ElianaCepeda <eli.cepedagonzalez@gmail.com>
2024-05-08 11:37:46 -0500

    Crear Cuenta arreglo de validaciones

--- src/main/java/omega/sgb/gui/CrearCuentaGUI.java
+++ src/main/java/omega/sgb/gui/CrearCuentaGUI.java
@@ -2,13 +2,7 @@ package omega.sgb.gui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
 import omega.sgb.SingletonControladores;
 import omega.sgb.SingletonPantallas;
 import omega.sgb.control.ControladorLogIn;
