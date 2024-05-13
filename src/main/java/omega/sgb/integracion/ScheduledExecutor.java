92308a6e50299523f4ba7caebed77523f2946f1a
Juando26030 <juando02603@gmail.com>
2024-05-13 17:59:03 -0500

    ejecutar cada 10 segundos clase lista sin funcionar v2

--- src/main/java/omega/sgb/integracion/ScheduledExecutor.java
+++ src/main/java/omega/sgb/integracion/ScheduledExecutor.java
@@ -9,7 +9,7 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class ScheduledExecutor {
-    private static ControladorActualizarApp controladorActualizarApp;
+    private static final ControladorActualizarApp controladorActualizarApp;
 
     static {
         try {
