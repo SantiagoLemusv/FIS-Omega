beb41432eca0cd8f11aedd85845a9f6b14f61158
Camila Neiza <castronjulianac@javeriana.edu.co>
2024-05-19 11:49:36 -0500

    cambios controladoresTest2

---
+++ src/test/java/omega/sgb/control/ControladorActualizarAppTest.java
@@ -0,0 +1,16 @@
+package omega.sgb.control;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControladorActualizarAppTest {
+
+    @Test
+    void validarReservasVencidas() {
+    }
+
+    @Test
+    void generarMultaParaPrestamo() {
+    }
+}
